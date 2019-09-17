60aed154bf4be8639ac4be0686e053d050d9bda8
Alexander Maja <alex-maja@hotmail.com>
2019-09-17 16:10:14 +0300

    Exercise 3.4, functional
    Delete functionality added

--- index.js
+++ index.js
@@ -38,6 +38,12 @@ app.get('/info/', (req, res) => {
     new Date + "</p></div>")
 })
 
+app.delete('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
+    persons = persons.filter(p => p.id !== id)
+    res.status(204).end()
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
